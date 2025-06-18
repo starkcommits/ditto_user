8e9586058fcf8e332e24de16919b7c60089d7ba8
pr0secutor <ifahim3107@gmail.com>
2025-06-18 18:53:25 +0530

    init

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}", // adjust based on your project
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
